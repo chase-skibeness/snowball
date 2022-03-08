2e3bdc3b5755c3b351812c1d538497bee8fd917c
Chase Skibeness <chase.skibeness@limeade.com>
2022-03-07 16:43:05 -0800

    start?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Accounts from "./Accounts";
 
 function App() {
+  let [accounts, updateAccounts] = useState([
+    {
+      "name": "BECU Line of Credit",
+      "balance Due": 2887.63,
+      "minimum Payment Due": 25.00,
+      "APR": 10.90
+    },
+    {
+      "name": "BECU Credit Card",
+      "balance Due": 17094.00,
+      "minimum Payment Due": 366.00,
+      "APR": 10.99
+    },
+    {
+      "name": "Capital One",
+      "balance Due": 2755.71,
+      "minimum Payment Due": 60.00,
+      "APR": 21.99
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accounts accounts={accounts} updateAccounts={updateAccounts} />
     </div>
   );
 }
